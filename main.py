20e2ca447a361ad2a5b58793b232bc340069bfde
BradyE3 <124845013+BradyE3@users.noreply.github.com>
2023-02-08 18:18:45 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+import pandas
+
+data = pandas.read_csv("alph-to-morse.csv")
+morse_dict = {row.letter: row.morse for (index, row) in data.iterrows()}
+
+user_input = input("What would you like translated to morse code: ").upper()
+
+
+def aplh_to_morse(user_txt):
+    coded_msg = ''
+    try:
+        for letter in user_txt:
+            if letter == ' ':
+                coded_msg += '/ '
+            else:
+                coded_msg += morse_dict[letter] + ' '
+
+    except KeyError:
+        print("Sorry only letters or numbers")
+
+    else:
+        print(coded_msg)
+
+
+aplh_to_morse(user_input)
+
+
+
+
+
+
+
+
